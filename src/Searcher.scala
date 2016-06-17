504524a17299ed7521cbd9ec8ac9199746aafb67
Ikaro <ikaroalef@gmail.com>
2016-06-17 14:31:04 -0300

    implementação de Menu para o Searcher

--- src/Searcher.scala
+++ src/Searcher.scala
@@ -1,4 +1,4 @@
-
+import scala.util.control.Breaks._
 import scala.collection.mutable.ListBuffer
 
 object Searcher {
@@ -12,11 +12,24 @@ object Searcher {
   }
   
   def main(args: Array[String]) {
+    var op = 1
     println("Informe o caminho do Diretório:")
     var path = scala.io.StdIn.readLine()
     LerDiretorio.walk(path).toList.foreach { e => println(e) }
-    println("Que busca deseja realizar? \n 1- Palavra Exata \n 2-Palavra contém a pesquisa ")
-    var busca = scala.io.StdIn.readLine()
-    searchCont(LerDiretorio.walk(path),busca).toList.foreach { e => println(e) }
+    while (op!=0){
+      println("\nQue busca deseja realizar? \n 1- Palavra Exata \n 2- Palavra contém a pesquisa \n 0- Sair")
+      op = scala.io.StdIn.readInt()
+      op match{
+        case 1 => 
+            print("Digite a palavra exata: ")
+            var busca = scala.io.StdIn.readLine()
+            search(LerDiretorio.walk(path),busca).toList.foreach { e => println(e) }
+        case 2 =>
+            print("Digite a palavra ou parte dela: ")
+            var busca = scala.io.StdIn.readLine()
+            searchCont(LerDiretorio.walk(path),busca).toList.foreach { e => println(e) }
+        case 0 => 
+      }
+    }
   }
 }
