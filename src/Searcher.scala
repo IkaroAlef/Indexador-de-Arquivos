420645f2e008ca2af55d57462027a43c534c5e9c
Ikaro <ikaroalef@gmail.com>
2016-06-17 14:11:51 -0300

    Implementação da pesquisa Contém

--- src/Searcher.scala
+++ src/Searcher.scala
@@ -7,12 +7,16 @@ object Searcher {
       table.filter{ case (_,e,_) => e.equals(busca) }
   }
   
+  def searchCont(table: ListBuffer[Any], busca: String): ListBuffer[Any] = {
+      table.filter{ case (_,e,_) => e.asInstanceOf[String].contains(busca) }
+  }
+  
   def main(args: Array[String]) {
     println("Informe o caminho do Diretório:")
     var path = scala.io.StdIn.readLine()
     LerDiretorio.walk(path).toList.foreach { e => println(e) }
-    println("O que deseja buscar?")
+    println("Que busca deseja realizar? \n 1- Palavra Exata \n 2-Palavra contém a pesquisa ")
     var busca = scala.io.StdIn.readLine()
-    search(LerDiretorio.walk(path),busca).toList.foreach { e => println(e) }
+    searchCont(LerDiretorio.walk(path),busca).toList.foreach { e => println(e) }
   }
 }
