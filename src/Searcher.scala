fedb0c51e342ba3df20890b8c89e9195844be5e1
Ikaro <ikaroalef@gmail.com>
2016-06-17 16:12:50 -0300

    Implementação searchOr (Pesquisa vários termos)

--- src/Searcher.scala
+++ src/Searcher.scala
@@ -11,23 +11,32 @@ object Searcher {
       table.filter{ case (_,e,_) => e.asInstanceOf[String].contains(busca) }
   }
   
+  def searchOr(table: ListBuffer[Any], busca: String): ListBuffer[Any] = {
+   table.filter{ case (_,e,_) => busca.contains(e.asInstanceOf[String]) }
+  }
+  
   def main(args: Array[String]) {
     var op = 1
+    var busca = ""
     println("Informe o caminho do Diretório:")
-    var path = scala.io.StdIn.readLine()
+    var path = "C:\\Users\\Ikaro\\Documents\\Exemplo"
     LerDiretorio.walk(path).toList.foreach { e => println(e) }
     while (op!=0){
-      println("\nQue busca deseja realizar? \n 1- Palavra Exata \n 2- Palavra contém a pesquisa \n 0- Sair")
+      println("\nQue busca deseja realizar? \n 1- Palavra Exata \n 2- Palavra contém a pesquisa \n 3- Pesquisa uma ou outra palavra \n 0- Sair")
       op = scala.io.StdIn.readInt()
       op match{
         case 1 => 
             print("Digite a palavra exata: ")
-            var busca = scala.io.StdIn.readLine()
+            busca = scala.io.StdIn.readLine()
             search(LerDiretorio.walk(path),busca).toList.foreach { e => println(e) }
         case 2 =>
             print("Digite a palavra ou parte dela: ")
-            var busca = scala.io.StdIn.readLine()
+            busca = scala.io.StdIn.readLine()
             searchCont(LerDiretorio.walk(path),busca).toList.foreach { e => println(e) }
+        case 3 =>
+            print("Digite as palavras: ")
+            busca = scala.io.StdIn.readLine()
+            searchOr(LerDiretorio.walk(path),busca).toList.foreach { e => println(e) }
         case 0 => 
       }
     }
