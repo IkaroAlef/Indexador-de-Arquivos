601b859f8c3c7c263158b05e626d011033f8b31f
Ikaro <ikaroalef@gmail.com>
2016-06-27 16:07:34 -0300

    -correções para forçar o usuário a indexar caso o banco nao exista
    -correção para pesquisa ignorar maiúscula e minúscula

--- src/Searcher.scala
+++ src/Searcher.scala
@@ -9,20 +9,30 @@ object Searcher {
   def search(table: ListBuffer[Any], busca: String): ListBuffer[Any] = {
     //list será uma list de cada palavra da busca, onde o token será espaços (usando Exp Reg)
     //checa se em list existe a palavra indexada
-    val list = busca.split("\\s+")
-    table.filter { case (_, e, _) => list.exists(e.asInstanceOf[String].contains) }
+    val list = busca.toLowerCase().split("\\s+")
+    table.filter { case (_, e, _) => list.exists(e.asInstanceOf[String].toLowerCase().contains) }
   }
 
   def main(args: Array[String]) {
     var op = -1
     var f = new File("index.db")
     val loop = new Breaks
-    breakable {
-      while (op != 0) {
+   
+    if (!(f.exists())) {
+      println("Index não existe.\nPor gentileza, informe um caminho do diretório para realizar a indexação")
+      var path = scala.io.StdIn.readLine()
+      println("Salvando a indexação no BD, aguarde um momento...")
+      var db = new DB()
+      db.startTabela()
+      db.inserirListBuffer(LerDiretorio.walk(path))  
+      println("Salvo com sucesso!")
+    }
+
+    while (op != 0) {
         println("\nO que deseja fazer? \n 1 - Indexar um diretório \n 2 - Realizar uma busca \n 0 - Sair")
         op = -1
         try{
-        op = scala.io.StdIn.readInt()
+          op = scala.io.StdIn.readInt()
         }catch {
           case e: NumberFormatException => println("Digite apenas numeros.") 
         }
@@ -49,13 +59,12 @@ object Searcher {
             var busca = scala.io.StdIn.readLine()
             search(listaBufDb, busca).toList.foreach { e => println(e) }
 
-          case 0 => break
+          case 0 => 
           case everythingElse => println("Opção invalida")
 
           //listaDb.foreach { e => println(e) }
           //LerDiretorio.walk(path).toList.foreach { e => println(e) }
         }
       }
-    }
   }
 }
